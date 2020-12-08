5e387978a050317d2a37d76250a743259adada36
benjaminfan <ben.yan.fan@gmail.com>
2020-12-07 23:13:19 -0600

    Fixing setup regulations

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="scalding",  # Replace with your own username
-    version="0.0.0",
+    name="scalding",
+    version="0.0.1",
     author="social-learning",
     author_email="ben.yan.fan@gmail.com",
     description="Scalding - functional data processing in python based off Scala & Hadoop",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/social-learning/setup.py",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
